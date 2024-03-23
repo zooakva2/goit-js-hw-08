ab554533b617227bfa380364f0d9f1524ce5b234
Zooakva <zooakva2@gmail.com>
2024-03-23 19:59:34 +0100

    correct

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,3 @@
-// JavaScript
 const images = [
   {
     preview:
@@ -14,34 +13,97 @@ const images = [
       "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
     description: "Container Haulage Freight",
   },
-  // Add more images here
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
 ];
 
 const galleryContainer = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery-container");
 
-const createGalleryItem = ({ preview, original, description }) => {
-  return `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img class="gallery-image" src="${preview}" data-source="${original}" alt="${description}" />
-      </a>
-    </li>
-  `;
-};
+const galleryItemsMarkup = createGalleryItemsMarkup(images);
 
-const createGalleryMarkup = images.map(createGalleryItem).join("");
-galleryContainer.insertAdjacentHTML("beforeend", createGalleryMarkup);
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
 
-galleryContainer.addEventListener("click", (event) => {
+function createGalleryItemsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery-item">
+                <a class="gallery-link" href="${original}">
+                    <img
+                        class="gallery-image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>
+        `;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
   event.preventDefault();
 
   const target = event.target;
-  if (target.tagName !== "IMG") return;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
 
   const largeImageUrl = target.dataset.source;
   const instance = basicLightbox.create(`
-    <img src="${largeImageUrl}" width="800" height="600">
-  `);
+        <img src="${largeImageUrl}" width="800" height="600">
+    `);
 
   instance.show();
-});
+}
